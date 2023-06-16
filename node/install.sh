9f1835fd71d033662203a32b567afa0d3e1f75dc
Lidi(김기현) <kihyun.kim@enlighten.kr>
2023-06-16 18:08:50 +0900

    fix: node 설정 스크립트 수정

--- node/install.sh
+++ node/install.sh
@@ -1,5 +1,10 @@
 export NVM_DIR=~/.nvm
-source $(brew --prefix nvm)/nvm.sh
+
+echo 'export NVM_DIR="$HOME/.nvm"
+          [ -s "$HOMEBREW_PREFIX/opt/nvm/nvm.sh" ] && \. "$HOMEBREW_PREFIX/opt/nvm/nvm.sh" # This loads nvm
+          [ -s "$HOMEBREW_PREFIX/opt/nvm/etc/bash_completion.d/nvm" ] && \. "$HOMEBREW_PREFIX/opt/nvm/etc/bash_completion.d/nvm" # This loads nvm bash_completion' >> ~/.zshrc
+
+source ~/.zshrc
 
 nvm install 14
 nvm install 16
